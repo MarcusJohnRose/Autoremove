ffaaa0a18a8cead6cff1537c3a0f3386b178a2d6
Marcus <marcus.rose@system1group.com>
2023-04-18 19:24:43 +0100

    Tracker overrides
    added tracker overrides for ratios and time limits

--- main.py
+++ main.py
@@ -22,13 +22,27 @@ qb_client = QBTorrentClient(
 qb_client.connect()
 
 # Initialize database
-db = TorrentDatabase(db_file='torrents.db')
+db = TorrentDatabase(db_file=os.path.join(absolute_path,'torrents.db'))
+
+def check_tracker(list,torrent):
+    for tracker in list:
+        if tracker["URL"] in torrent.tracker:
+            return True
+    return False
+
+
+def get_tracker_rules(list,torrent):
+    for tracker in list:
+        if tracker["URL"] in torrent.tracker:
+            return [tracker["min_ratio"],tracker["min_age"]]
+
 
 # Get torrents for each category and delete old ones
 for category in config['categories']:
     torrents = qb_client.get_torrents_by_category(category)
-    age_limit_hours = config['categories'][category]['max_age_hours']
-    min_ratio = config['categories'][category]['min_ratio']
+    category_age_limit_hours = config['categories'][category]['max_age_hours']
+    category_min_ratio = config['categories'][category]['min_ratio']
+    
     for torrent in torrents:
         ageMet = False
         ratioMet = False
@@ -36,27 +50,25 @@ for category in config['categories']:
         added_on = datetime.datetime.fromtimestamp(torrent.added_on)
         age_in_hours = (now - added_on).total_seconds() / 3600
         
+        if  check_tracker(config["trackers"],torrent):
+            tracker_miniumss = get_tracker_rules(list,torrent)
+            min_age = tracker_miniumss(0)
+            min_ratio = tracker_miniumss(1)
+        else:
+            min_age = category_age_limit_hours
+            min_ratio = category_min_ratio
+
         #Check age vs age limit
-        if age_in_hours > age_limit_hours:
+        if age_in_hours > min_age:
             ageMet = True 
         
         #Check ratio vs ratio limit
         ratio=torrent.ratio
-        if ratio > min_ratio:
+        if ratio >= min_ratio:
             ratioMet = True
         
         # Delete torrent and save information to database
         if ratioMet and ageMet:
-            torrentName=torrent.name
-            torrentSize=torrent.size
-            torrentRatio=torrent.ratio
-            torrentAdded=added_on
-            torrentRemoved_on=now
-            torrentAge_when_removed=age_in_hours
-            torrentTracker=torrent.tracker
-            torrentCategory=category
-   
-            qb_client.delete_torrent(True,torrent.hash)
-            
-            db.insert_torrent(name=torrentName,size=torrentSize,ratio=torrentRatio,added=torrentAdded,removed_on=torrentRemoved_on,age_when_removed=torrentAge_when_removed,tracker=torrentTracker,category=torrentCategory)
-            
+            qb_client.delete_torrent(True,torrent.hash)    
+            db.insert_torrent(name=torrent.name,size=torrent.size,ratio=torrent.ratio,added=added_on,removed_on=now,age_when_removed=age_in_hours,tracker=torrent.tracker,category=category)
+
